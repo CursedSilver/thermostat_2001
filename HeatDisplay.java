6d7ea3de8e2b24fe87465f354fbc819c594d5cde
Johnny <jrgillespie@mun.ca>
2018-09-21 16:08:11 -0230

    Initial commit

---
+++ HeatDisplay.java
@@ -0,0 +1,64 @@
+
+/**
+ * Write a description of class HeatDisplay here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class HeatDisplay
+{
+    // instance variables 
+    private int currentTemperature, minTemperature, maxTemperature, increment;
+
+    /**
+     * Constructor for objects of class HeatDisplay
+     */
+    public HeatDisplay(int max, int min, int incr)
+    {
+        // initialise instance variables
+
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int getCurrentTemperature()
+    {
+        // put your code here
+
+    }
+    
+    public int convertToFahrenheit()
+    {
+        // put your code here
+
+    }
+    
+    public void warmerTemp()
+    {
+        // put your code here
+ 
+    }
+    
+    public void coolerTemp()
+    {
+        // put your code here
+ 
+    }
+    
+    public void setIncrement(int incr)
+    {
+        // put your code here
+ 
+    }
+    
+    public String getTemperature(String c_or_f)
+    {
+        // put your code here
+
+        
+    }
+}
